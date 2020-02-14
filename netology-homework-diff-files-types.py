afcf3840d6b53ccd40ab98edb479e2004b89d59b
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-02-15 02:18:35 +0300

    Final version

--- netology-homework-diff-files-types.py
+++ netology-homework-diff-files-types.py
@@ -1,39 +1,44 @@
 import json
-from pprint import pprint
+import operator
+from collections import Counter
+import xml.etree.ElementTree as ET
 
+
+def first_10_words(descriptions):
+    list_of_words = list()
+    words = str()
+    for description in descriptions:
+        # if type(description) == types.dict "<class 'dict'>":
+        if isinstance(description, dict):
+            words = str(description['description'])
+            words = words.split()
+        # elif type(description) == "<class 'xml.etree.ElementTree.Element'>":
+        else:
+            words = str(description.find("description").text)
+            words = words.split()
+        for word in words:
+            if len(word) > 6:
+                list_of_words.append(word)
+    list_of_words = Counter(list_of_words)
+    list_of_words = sorted(list_of_words.items(), key=operator.itemgetter(1), reverse=True)
+    first_10_count = 0
+    for word in list_of_words:
+        if first_10_count == 10:
+            break
+        else:
+            print(f'{first_10_count + 1} место: слово "{word[0]}" повторялось {word[1]} раз')
+            first_10_count += 1
+
+
+print('\nЧтение JSON файл:')
 data = dict()
 with open("newsafr.json", encoding="utf-8") as file:
     data = json.loads(file.read())
-
 descriptions = data['rss']['channel']['items']
-# pprint(descriptions)
-# pprint(descriptions[0])
+first_10_words(descriptions)
 
-list_of_words = list()
-news_count = 0
-while news_count < len(descriptions):
-    full_string = str()
-    for description in descriptions:
-        add_string = str(description['description'])
-        # if not full_string:
-        #     full_string = add_string
-        # else:
-        #     full_string = full_string + ' ' + add_string
-        add_string = add_string.split()
-        for word in add_string:
-            if len(word) > 6:
-                list_of_words.append(word)
-        # print(description['description'])
-    # pprint(descriptions[news_count])
-    news_count += 1
-# pprint(full_string)
-# full_string = full_string.split()
-# pprint(full_string)
-print(list_of_words)
-
-# for description in data['rss']['channel']['items'][0]:
-#     for key, value in description:
-#         if key == 'description':
-#             pprint(value)
-# pprint(data['description'], width=100)
-# pprint(data['rss']['channel']['items'], width=100)
+print('\nЧтение XML файла:')
+data = ET.parse("newsafr.xml")
+data_root = data.getroot()
+descriptions = data_root.findall(r"channel/item")
+first_10_words(descriptions)
